3f07866d7ba4fc986983c439e85285e657c67f67
Isabella Miranda <isabellamiirandam@gmail.com>
2023-10-26 14:38:34 -0300

    adicionando evento click e criando a lógica

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const firstNumberElement = document.querySelector('#firstNumber')
 const secondNumberElement = document.querySelector('#secondNumber')
 const buttonElement = document.querySelector('.send')
 
+buttonElement.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const firstNumber = Number(firstNumberElement.value)
+  const secondNumber = Number(secondNumberElement.value)
+
+  const sum = firstNumber + secondNumber
+  const subtraction = firstNumber - secondNumber
+  const multiplication = firstNumber * secondNumber
+  const division = firstNumber / secondNumber
+
+  const result = ` Seu resultado final é :
+  Soma: ${sum}
+  Subtração: ${subtraction}
+  Multiplicação: ${multiplication}
+  Divisão: ${division}`
+
+  alert(result)
+
+})
