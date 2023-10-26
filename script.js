647a606c4a7f4555f4ea37daf0027773623d7932
Isabella Miranda <isabellamiirandam@gmail.com>
2023-10-26 14:20:02 -0300

    pegando os elementos html com seletor

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstNumberElement = document.querySelector('#firstNumber')
+const secondNumberElement = document.querySelector('#secondNumber')
+const buttonElement = document.querySelector('.send')
+
